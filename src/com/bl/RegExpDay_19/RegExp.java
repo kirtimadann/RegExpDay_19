11cd2e760dbc2925eaca6b5f4233249c401a0311
lunainfotec <kirtimadan1997@gmail.com>
2023-05-02 15:41:25 +0530

    added : master

---
+++ src/com/bl/RegExpDay_19/RegExp.java
@@ -0,0 +1,18 @@
+package com.bl.RegExpDay_19;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+public class RegExp {
+
+   static Pattern p = Pattern.compile("^[A-Z a-z]{3,}$");
+    public static void main(String[] args) {
+        String str = "Kirti";
+        Matcher m = p.matcher(str);
+        if (m.matches()){
+            System.out.println("valid");
+        }else {
+            System.out.println("invalid");
+        }
+
+
+    }
+}
